8deeb9b89644f14ffea92f540f3ddd2e3dc76e7d
Yasmin Marcelino <yasmarcelino91@gmail.com>
2022-07-14 15:08:32 -0300

    ueba

--- core/views.py
+++ core/views.py
@@ -1,8 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
 
-from core.models import Categoria
-from core.serializers import CategoriaSerializer
+from core.models import Categoria, Editora, Autor, Livro
+from core.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer
+
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
